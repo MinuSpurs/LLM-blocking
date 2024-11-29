870c9ff89b68770920fd09709d6a9b3b594bd745
polyn0 <jungahn00@gmail.com>
2024-11-29 16:32:18 +0900

    FIX: saving the kept and the discarded csv

--- json_preprocess.py
+++ json_preprocess.py
@@ -2,16 +2,18 @@ import json
 import spacy
 import pandas as pd
 from tqdm import tqdm
-from hunspell import HunSpell
+from hunspell import Hunspell
 import Levenshtein
 from nltk.corpus import words, wordnet
 import nltk
+import os
 
 #nltk.download('words')
 #nltk.download('wordnet')   # Remove comments as needed
 
 # Hunspell setup
-h = HunSpell('/usr/share/hunspell/en_US.dic', '/usr/share/hunspell/en_US.aff')
+# h = Hunspell('/usr/share/hunspell/en_US.dic', '/usr/share/hunspell/en_US.aff')
+h = Hunspell()
 
 with open('/usr/share/hunspell/en_US.dic', 'r') as file:
     dictionary = [line.strip() for line in file.readlines() if line.strip()]
@@ -58,18 +60,19 @@ def extract_data(data, output_file, query="is a made up word"):
 
     return df
 
-def save_filtered_and_removed(original_df, filtered_df, stage_name):    # 필터링에서 조건에 부합하지 않은(탈락한) 단어들 저장하는 함수
+
+def save_filtered_and_removed(original_df, filtered_df, stage_name):    
 
     removed_df = original_df[~original_df.index.isin(filtered_df.index)]
     
-    filtered_df.to_csv(f"./data/csv/{stage_name}_filtered.csv", index=False)    # 각 단계별로 저장되고 탈락된 단어들이 모아짐
+    filtered_df.to_csv(f"./data/csv/{stage_name}_filtered.csv", index=False)    
     removed_df.to_csv(f"./data/csv/{stage_name}_removed.csv", index=False)
     
     print(f"{stage_name}: Filtered saved to {stage_name}_filtered.csv, Removed saved to {stage_name}_removed.csv")
 
 
 
-# csv filtering - only EN, length>3, not nltk word
+# csv filtering - only EN, length>3, not nltk word, wordnet
 def filter_words(df):
     english_words = set(words.words())
 
@@ -80,12 +83,13 @@ def filter_words(df):
 
     return filtered3
 
-def filter_archs(df, target_word):
+def filter_archs(df):
+
     not_words = []
 
-    for _, row in df.iterrows():
+    for i, row in tqdm(df.iterrows()):
         sentence = row['sentence']
-        doc_ix = row['doc_ix']
+        target_word = row['expression'].split()[-1]
         doc = nlp(sentence)
 
         for token in doc:
@@ -98,78 +102,87 @@ def filter_archs(df, target_word):
                     if is_token_index > 0:
                         x_token = doc[is_token_index - 1]  # Get the token representing "X"
                         x_word = x_token.text
+                        
+                        # Check for right arcs
+                        has_right_arcs = any(child.i > token.i for child in token.children)
+                        if not has_right_arcs:
+                            # Only add if there are no right arcs
+                            not_words.append(i)
 
-                        # Append the word, sentence, and doc_ix to the result
-                        # print(f"Adding '{x_word}' to not_words list.")  # Handle comment if necessary
-                        not_words.append((x_word, sentence, doc_ix)) 
+    return df.loc[not_words]
 
-    filtered_df = pd.DataFrame(not_words, columns=['word', 'sentence', 'doc_ix'])
-    return filtered_df
 
 def filter_hunspell(df):
-    filtered_words = []
-    for _, row in df.iterrows():
-        word = row['word']
-        if not h.spell(word):  # If the word is not in the Hunspell dictionary
-            filtered_words.append(row)
-
-    filtered_df = pd.DataFrame(filtered_words, columns=['word', 'sentence', 'doc_ix'])
-    return filtered_df
+    df = df[df['word'].apply(lambda x : True if not h.spell(x) else False)].copy()
+    return df
 
 
 def filter_levenshtein(df, dictionary, threshold=2):
-    filtered_words = []
-    for _, row in df.iterrows():
-        word = row['word']
+
+    flags = []
+    for word in df['word']:
+        
         closest_word = min(dictionary, key=lambda x: Levenshtein.distance(word, x))
         distance = Levenshtein.distance(word, closest_word)
-        if distance > threshold:
-            filtered_words.append(row)
 
-    filtered_df = pd.DataFrame(filtered_words, columns=['word', 'sentence', 'doc_ix'])
-    return filtered_df
+        if distance > threshold:
+            flags.append(True)
+        else:
+            flags.append(False)
+
+    return df[flags]
 
 
 def main():
     """
     Main function to process JSON data and filter words.
     """
-    total = []
-    expressions = ['is a created word']  # Add more expressions as needed
 
-    for expression in expressions:
-        target_word = expression.split()[-1]
+    # Combine JSON files grouped by expressions into a single CSV file.
+    csv_path = f"./data/csv/total.csv"
+    if not os.path.isfile(csv_path):
 
-        json_path = "/home/work/jupyter/minwoo/CMU/LLM_blocking/data/json/is_a_created_word.json"
-        csv_path = f"./data/csv/{'_'.join(expression.split())}.csv"
+        total = []
+        expressions = ['is a created word', 'is a made up word']  # Add more expressions as needed
+        # expressions = ['is a made up word', 'is not a common word', 'is a created word', 'is an invented word']
+        for expression in expressions:
+            
 
-        with open(json_path, "r") as file:
-            data = json.load(file)
-            print(f"Data count for '{expression}' : \t{len(data)}")
+            json_path = f"./data/json/{'_'.join(expression.split())}.json"
+            csv_path = f"./data/csv/{'_'.join(expression.split())}.csv"
 
-        df = extract_data(data, csv_path, query=expression)
+            with open(json_path, "r") as file:
+                data = json.load(file)
+                print(f"Data count for '{expression}' : \t{len(data)}")
 
-        df1 = filter_words(df)
-        save_filtered_and_removed(df, df1, "filter_words")
-        
-        df2 = filter_archs(df1, target_word)
-        save_filtered_and_removed(df1, df2, "filter_archs")
-        
-        df3 = filter_hunspell(df2)
-        save_filtered_and_removed(df2, df3, "filter_hunspell")
-        
-        df4 = filter_levenshtein(df3, dictionary)
-        save_filtered_and_removed(df3, df4, "filter_levenshtein")
+            df = extract_data(data, csv_path, query=expression)
+            df['expression'] = [expression] * len(df)
+            
+            total.append(df)
 
-        df4.to_csv(f"{csv_path.split('.csv')[0]}_filtered.csv", index=False)
-        print(f"Saving filtered CSV with len {len(df4)}")
+        total_df = pd.concat(total, ignore_index=True)
+        total_df.to_csv(csv_path, index=False)
+        print(f"Total data saved to {csv_path} of length {len(total_df)}")
 
-        total.append(df4)
 
-    total_df = pd.concat(total, ignore_index=True)
-    csv_path = f"./data/words/total_non_words.csv"
-    total_df.to_csv(csv_path, index=False)
-    print(f"Total filtered words saved to {csv_path}")
+    # Word filtering
+
+    df = total_df.copy()
+    df1 = filter_words(df)
+    save_filtered_and_removed(df, df1, "filter_words")
+    
+    
+    df2 = filter_archs(df1)
+    save_filtered_and_removed(df1, df2, "filter_archs")
+    
+    df3 = filter_hunspell(df2)
+    save_filtered_and_removed(df2, df3, "filter_hunspell")
+    
+    df4 = filter_levenshtein(df3, dictionary)
+    save_filtered_and_removed(df3, df4, "filter_levenshtein")
+
+    df4.to_csv(f"./data/words/total_non_words.csv", index=False)
+    print(f"Saving filtered CSV with len {len(df4)}")
 
 
 main()
